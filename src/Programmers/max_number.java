7cb7d6dc964601fbca88b586aeb92636f3264588
dusgh <dusgh@192.168.0.104>
2021-12-21 14:07:59 +0900

    프로그래머스_정렬_가장큰수

--- src/Programmers/max_number.java
+++ src/Programmers/max_number.java
@@ -4,13 +4,14 @@ public class max_number {
 	public static void main(String args[]) {
 		int []numbers1= {6,2,10};
 		int []numbers2= {3, 30, 34, 5, 9};
-		System.out.println(max_number(numbers1));
+		System.out.println(max_number(numbers2));
 	}
+	/*//숫자를 아스키코드로 변환하지 않고 INT 타입으로 비교시 실패
 	  public static String max_number(int[] numbers) {
 	        String answer = "";
 	        ArrayList<Integer>list=new ArrayList<Integer>();
 	        ArrayList<Integer>nums=new ArrayList<Integer>();
-	        
+
 	        for(int num:numbers) {
 	        	nums.add(num);
 	        }
@@ -45,4 +46,25 @@ public class max_number {
 	        }
 	        return answer;
 	    }
+	 */
+	//compare to 사용(Collections.sort 와 달랐음)
+	public static String max_number(int[] numbers) {
+		String answer = "";
+		ArrayList<String>list=new ArrayList<String>();
+
+		for(int num:numbers) {
+			list.add(Integer.toString(num));
+		}
+		Collections.sort(list, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+		if(list.get(0).equals("0")) {
+			answer="0";
+		}else {
+			for(int i=0;i<list.size();i++) {
+				answer+=list.get(i);
+			}
+		}
+
+		return answer;
+	}
 }
